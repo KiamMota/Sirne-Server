7249e779d9cb361ee9cba90e99bf1d7169657a22
KiamMota <somentekiam@gmail.com>
2025-07-24 14:45:15 -0300

    add : Domain

--- include/Domain/Lanche/Lanche.hpp
+++ include/Domain/Lanche/Lanche.hpp
@@ -1,19 +1,26 @@
 #ifndef _LANCHE_H_
 #define _LANCHE_H_
-#include "Domain/Lanche/Vo/Calorias.hpp"
+
 #include <string>
 
 class Lanche
 {
 	private:
+		long id;
 		std::string NomeLanche;
+		int	calorias;
+		bool lactose;
 	public:
-		Lanche(const char* NomeLanche)
+		Lanche(const char* NomeLanche, int calorias)
 		{
 			this->NomeLanche = NomeLanche;
-
+			this->calorias = calorias;	
 		}
 
+		int GetId()						{return id;}
+		std::string GetNome() {return NomeLanche;}
+		int GetCalorias()			{return calorias;}
+		bool GetLactose()			{return lactose;}
 };
 
 
